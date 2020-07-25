0687e54bf9081cb1e67326aa537a123d21fead44
wilsonng21 <wilsongch92@gmail.com>
2020-07-25 10:32:20 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_results = data.each_with_object({}) do |(key, value), final_hash|
+    value.each do |inner_key, pigeons|
+      pigeons.each do |pigeon|
+        if !final_hash[pigeon]
+          final_hash[pigeon] = {}
+        end
+        if !final_hash[pigeon][key]
+          final_hash[pigeon][key] = []
+        end
+        final_hash[pigeon][key].push(inner_key.to_s)
+      end
+    end
+  end
 end
