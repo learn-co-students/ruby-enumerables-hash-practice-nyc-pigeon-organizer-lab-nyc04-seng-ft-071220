0207e9b1c6e816ad0abeeb9dd15180a953682da6
fbado66 <fabdo66@gmail.com>
2020-07-26 16:54:17 -0400

    Pigeon Organizer Lab

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object ({}) do |(outer_key, value), pigeon_names|
+    value.each do |inner_keys, names|
+      names.each do |name|
+
+        if !pigeon_names.include?(name)
+          pigeon_names[name] = {}
+        end
+
+        if !pigeon_names[name].include?(outer_key)
+          !pigeon_names[name][outer_key] = []
+        end
+
+        pigeon_names[name][outer_key] << inner_keys.to_s
+
+        # binding.pry
+      end     
+    end 
+  end 
+  # binding.pry
 end
