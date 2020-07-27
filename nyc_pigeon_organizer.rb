95e107187ae572b8c90f5bbb3386686aa38b2924
sbalgobin94 <sbalgobin94@gmail.com>
2020-07-27 14:40:21 -0400

    finished lab

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,41 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Create an empty array of pidgeon names
+  names = []
+
+  # Add the values (names) from the 'male' and 'female' keys to the pidgeon name array
+  for name in data[:gender][:male] + data[:gender][:female]
+    names << name
+  end
+
+  # Create a new hash with each name as a key using the hash and collect method
+  organized_data = Hash[names.collect { |name| [name, {:color => [], :gender => [], :lives => []}] } ]
+
+  data[:color].each do |color, names|
+    names.each do |name|
+      if organized_data.key?(name)
+        organized_data[name][:color] << color.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |gender, names|
+    names.each do |name|
+      if organized_data.key?(name)
+        organized_data[name][:gender] << gender.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |lives, names|
+    names.each do |name|
+      if organized_data.key?(name)
+        organized_data[name][:lives] << lives.to_s
+      end
+    end
+  end
+  
+  organized_data
+
 end
+
+
